2b00445f99b058c07b10c301ca4b54bfdd6a460a
singabytes <pbontemps@yahoo.com>
2022-12-17 14:51:08 +0800

    cleanup

--- app_server/routes/index.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-var controllerMain = require('../controllers/main')
-
-// homepageControllers is the callback function that
-// implements the application logic of the homepage
-router.get('/', controllerMain.index);
-
-module.exports = router;
